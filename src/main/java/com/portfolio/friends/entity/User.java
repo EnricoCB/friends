6d0a70fa8f702fa77e8f3a5528cccb99cf9db565
EnricoCB <enriv.budel@gmail.com>
2024-11-21 16:35:02 -0400

    Adicionando entidade User e implementando UserDetails

--- src/main/java/com/portfolio/friends/entity/User.java
+++ src/main/java/com/portfolio/friends/entity/User.java
@@ -1,4 +1,78 @@
 package com.portfolio.friends.entity;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 10)
+    private String username;
+    private String password;
+
+    @Column(nullable = false)
+    private LocalDate signupDate = LocalDate.now();
+
+    @Enumerated(EnumType.STRING)
+    private ProfileVisibility visibility;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public enum ProfileVisibility {
+        HIDDEN, PUBLIC
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
--- src/main/java/com/portfolio/friends/service/UserService.java
+++ src/main/java/com/portfolio/friends/service/UserService.java
@@ -1,4 +1,14 @@
 package com.portfolio.friends.service;
 
+import com.portfolio.friends.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
